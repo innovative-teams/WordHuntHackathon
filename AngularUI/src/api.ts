1a304d908958475eb257c18a7dbc2510096d9f02
ismailkaygisiz <kaygisizismail24@gmail.com>
2022-02-01 19:22:51 +0300

    Refactored

--- AngularUI/src/api.ts
+++ AngularUI/src/api.ts
@@ -1,8 +1,8 @@
-// export const apiUrl = 'https://wordhunthackathon.somee.com/api/'; // project api url
-// export const baseUrl = 'https://wordhunthackathon.somee.com/'; // project url
-// export const baseUrlForImage = 'https://wordhunthackathon.somee.com'; // project url without /
+export const apiUrl = 'https://wordhunthackathon.somee.com/api/'; // project api url
+export const baseUrl = 'https://wordhunthackathon.somee.com/'; // project url
+export const baseUrlForImage = 'https://wordhunthackathon.somee.com'; // project url without /
 
-export const apiUrl = 'https://localhost:5001/api/'; // project api url
-export const baseUrl = 'https://localhost:5001/'; // project url
-export const baseUrlForImage = 'https://localhost:5001'; // project url without /
-export var translates: any = {};
+// export const apiUrl = 'https://localhost:5001/api/'; // project api url
+// export const baseUrl = 'https://localhost:5001/'; // project url
+// export const baseUrlForImage = 'https://localhost:5001'; // project url without /
+// export var translates: any = {};
